13f940e210f361285766e855518030c37927962d
Junichi Tokuda <tokuda@bwh.harvard.edu>
2011-08-18 12:37:15 -0400

    ENH: adding capability message type

---
+++ Source/igtlutil/igtl_capability.c
@@ -0,0 +1,59 @@
+/*=========================================================================
+
+  Program:   OpenIGTLink Library
+  Module:    $HeadURL: $
+  Language:  C
+  Date:      $Date: 2010-07-21 14:15:29 -0400 (Wed, 21 Jul 2010) $
+  Version:   $Revision: 6836 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "igtl_capability.h"
+#include "igtl_util.h"
+
+void igtl_export igtl_capability_init_info(igtl_capability_info * info)
+{
+}
+
+
+int igtl_export igtl_capability_alloc_info(igtl_capability_info * info, int ntypes)
+{
+}
+
+
+int igtl_export igtl_capability_free_info(igtl_capability_info * info)
+{
+}
+
+
+igtl_uint32 igtl_export igtl_capability_get_capability_length(igtl_capability_info * info)
+{
+}
+
+
+int igtl_export igtl_capability_unpack(int type, void * byte_array, igtl_capability_info * info, igtl_uint64 pack_size)
+{
+}
+
+
+int igtl_export igtl_capability_pack(igtl_capability_info * info, void * byte_array, int type)
+{
+}
+
+
+igtl_uint64 igtl_export igtl_capability_get_crc(igtl_capability_info, int type, void* capability)
+{
+}
+
+
+
+
---
+++ Source/igtlutil/igtl_capability.h
@@ -0,0 +1,107 @@
+/*=========================================================================
+
+  Program:   OpenIGTLink Library
+  Module:    $HeadURL: $
+  Language:  C
+  Date:      $Date: 2010-11-23 14:47:40 -0500 (Tue, 23 Nov 2010) $
+  Version:   $Revision: 6958 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __IGTL_CAPABILITY_H
+#define __IGTL_CAPABILITY_H
+
+#include "igtl_win32header.h"
+#include "igtl_util.h"
+#include "igtl_types.h"
+#include "igtl_win32header.h"
+
+#define IGTL_CAPABILITY_HEADER_SIZE          4
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+  igtl_uint32      ntypes;
+  unsigned char ** typenames;
+} igtl_capability_info;
+
+
+/*
+ * Initialize igtl_ndarray_info
+ */
+void igtl_export igtl_capability_init_info(igtl_capability_info * info);
+
+
+/*
+ * Allocate / free ndarray info
+ *
+ * Allocate size array and ND-array pointed from igtl_ndarray_info.
+ * 'type' and 'dim' in igtl_ndarray_info must be specified before
+ * calling igtl_ndarray_alloc_info().
+ */
+
+int igtl_export igtl_capability_alloc_info(igtl_capability_info * info, int ntypes);
+int igtl_export igtl_capability_free_info(igtl_capability_info * info);
+
+
+/*
+ * Calculate capability data size
+ *
+ * This function calculates size of the pixel array, which will be
+ * transferred with the specified header.
+ */
+
+igtl_uint32 igtl_export igtl_capability_get_capability_length(igtl_capability_info * info);
+
+
+/*
+ * Unpack CAPABILITY message
+ *
+ * Extract information in a byte array of CAPABILITY messages and store
+ * it in a igtl_capability_info structure. 'type' argument specifies
+ * a message type prefix (none, GET_, STT_, STP_ or RTS_) by IGTL_TYPE_PREFIX_* macro.
+ * Returns 1 if success, otherwise 0.
+ */
+
+int igtl_export igtl_capability_unpack(int type, void * byte_array, igtl_capability_info * info, igtl_uint64 pack_size);
+
+
+/*
+ * Pack CAPABILITY message
+ *
+ * Convert an igtl_capability_info structure to a byte array. 
+ * 'byte_array' should be allocated prior to calling igtl_capability_pack() with memory size
+ * calculated by igtl_capability_get_size(). 'type' argument specifies a message type prefix
+ * (none, or GET_) by IGTL_TYPE_PREFIX_* macro. Returns 1 if success, otherwise 0.
+ */
+
+int igtl_export igtl_capability_pack(igtl_capability_info * info, void * byte_array, int type);
+
+
+/*
+ * CRC calculation
+ *
+ * This function calculates CRC of image data body including header
+ * and array of pixel data.
+ *
+ */
+
+igtl_uint64 igtl_export igtl_capability_get_crc(igtl_capability_info, int type, void* capability);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __IGTL_CAPABILITY_H */
+
+
+
+
