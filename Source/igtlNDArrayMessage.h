openigtlink/OpenIGTLink

commit efee6a8c936e54b2af9254e97f6f86b8235eed24

BUG: Fixing issue #8 -- Accessfucntions for m_Array and m_Type are missing.

Source/igtlNDArrayMessage.h
@@ -115,8 +115,8 @@ public:
 public:
 
   int         SetArray(int type, ArrayBase * a);
-  ArrayBase * GetArray();
-  int         GetType();
+  ArrayBase * GetArray() { this->m_Array; };
+  int         GetType()  { return this->m_Type; } ;
 
 protected:
   NDArrayMessage();

commit 788e1483f1e2b76707a946e029dedc920c81a345

BUG: Fix issue caused by the previous commit efee6a8c936e54b2af9254e97f6f86b8235eed24

Source/igtlNDArrayMessage.h
@@ -115,7 +115,7 @@ public:
 public:
 
   int         SetArray(int type, ArrayBase * a);
-  ArrayBase * GetArray() { this->m_Array; };
+  ArrayBase * GetArray() { return this->m_Array; };
   int         GetType()  { return this->m_Type; } ;
 
 protected: