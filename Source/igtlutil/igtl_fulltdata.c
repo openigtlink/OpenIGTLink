5e05346f707f783c47d4bda8dd92d93abb049968
Steve Thompson <s.thompson@ucl.ac.uk>
2013-10-31 20:09:16 +0000

    #47: separated tdata and full tdata

--- Source/igtlutil/igtl_fulltdata.c
+++ Source/igtlutil/igtl_fulltdata.c
@@ -15,12 +15,12 @@
 #include <string.h>
 #include <math.h>
 
-#include "igtl_tdata.h"
+#include "igtl_fulltdata.h"
 #include "igtl_util.h"
 
-void igtl_export igtl_tdata_convert_byte_order(igtl_tdata_element* tdatalist, int nitem)
+void igtl_export igtl_fulltdata_convert_byte_order(igtl_fulltdata_element* tdatalist, int nitem)
 {
-  igtl_tdata_element* elem;
+  igtl_fulltdata_element* elem;
   int i;
   int j;
   igtl_int32* tmp;
@@ -43,27 +43,9 @@ void igtl_export igtl_tdata_convert_byte_order(igtl_tdata_element* tdatalist, in
 }
 
 
-void igtl_export igtl_stt_tdata_convert_byte_order(igtl_stt_tdata* stt_tdata)
+igtl_uint64 igtl_export igtl_fulltdata_get_crc(igtl_fulltdata_element* tdatalist, int nitem)
 {
-  igtl_int32* tmp;
-
-  if (igtl_is_little_endian()) 
-    {
-    tmp = (igtl_int32*)&(stt_tdata->resolution);
-    *tmp = BYTE_SWAP_INT32(*tmp);
-    }
-}
-
-
-void igtl_export igtl_rts_tdata_convert_byte_order(igtl_rts_tdata* rts_tdata)
-{
-  // do nothing
-}
-
-
-igtl_uint64 igtl_export igtl_tdata_get_crc(igtl_tdata_element* tdatalist, int nitem)
-{
-  igtl_tdata_element* elem;
+  igtl_fulltdata_element* elem;
   int i;
   igtl_uint64  crc;
 
@@ -71,29 +53,10 @@ igtl_uint64 igtl_export igtl_tdata_get_crc(igtl_tdata_element* tdatalist, int ni
   for (i = 0; i < nitem; i ++)
     {
     elem = &(tdatalist[i]);
-    crc = crc64((unsigned char*) elem, IGTL_TDATA_ELEMENT_SIZE, crc);
+    crc = crc64((unsigned char*) elem, IGTL_FULLTDATA_ELEMENT_SIZE, crc);
     }
 
   return crc;
 }
 
-
-igtl_uint64 igtl_export igtl_stt_tdata_get_crc(igtl_stt_tdata* stt_tdata)
-{
-  igtl_uint64  crc;
-
-  crc = crc64(0, 0, 0);
-  crc = crc64((unsigned char*) stt_tdata, IGTL_STT_TDATA_SIZE, crc);
-  return crc;
-}
-
-
-igtl_uint64 igtl_export igtl_rts_tdata_get_crc(igtl_rts_tdata* rts_tdata)
-{
-  igtl_uint64  crc;
-
-  crc = crc64(0, 0, 0);
-  crc = crc64((unsigned char*) rts_tdata, IGTL_RTS_TDATA_SIZE, crc);
-  return crc;
-}
 
--- Source/igtlutil/igtl_fulltdata.h
+++ Source/igtlutil/igtl_fulltdata.h
@@ -12,26 +12,12 @@
 
 =========================================================================*/
 
-#ifndef __IGTL_TDATA_H
-#define __IGTL_TDATA_H
+#ifndef __IGTL_FULLTDATA_H
+#define __IGTL_FULLTDATA_H
 
-#include "igtl_win32header.h"
-#include "igtl_util.h"
-#include "igtl_types.h"
+#include "igtl_tdata.h"
 
-#define  IGTL_TDATA_ELEMENT_SIZE           74
-#define  IGTL_STT_TDATA_SIZE               36
-#define  IGTL_RTS_TDATA_SIZE               1
-
-#define  IGTL_TDATA_LEN_NAME               20  /* Maximum length of tracking instrument name */
-#define  IGTL_STT_TDATA_LEN_COORDNAME      32  /* Maximum length of coordinate system name */
-
-#define  IGTL_TDATA_TYPE_TRACKER           1  /* Tracker */
-#define  IGTL_TDATA_TYPE_6D                2  /* 6D instrument (regular instrument) */
-#define  IGTL_TDATA_TYPE_3D                3  /* 3D instrument (only tip of the instrument defined) */
-#define  IGTL_TDATA_TYPE_5D                4  /* 5D instrument (tip and handle are defined,
-                                                 but not the normal vector) */
-
+#define  IGTL_FULLTDATA_ELEMENT_SIZE       74
 
 #ifdef __cplusplus
 extern "C" {
@@ -46,45 +32,31 @@ typedef struct {
   igtl_uint8   reserved;       /* Reserved byte */
   igtl_float32 transform[12];  /* same as TRANSFORM */
   igtl_float32 error;          /* the tracking error */
-} igtl_tdata_element;
+} igtl_fulltdata_element;
 
-
-typedef struct {
-  igtl_int32   resolution;     /* Minimum time between two frames. Use 0 for as fast as possible. */
-                               /* If e.g. 50 ms is specified, the maximum update rate will be 20 Hz. */
-  char         coord_name[IGTL_STT_TDATA_LEN_COORDNAME]; /* Name of the coordinate system */
-} igtl_stt_tdata;
-
-typedef struct {
-  igtl_int8    status;         /* 0: Success 1: Error */
-} igtl_rts_tdata;
 
 #pragma pack()
 
 /** igtl_tdata_get_data_size(n) calculates the size of body based on the number
  *  of tdatas. The size of body is used in the message header.*/
-#define igtl_tdata_get_data_size(n)  ((n) * IGTL_TDATA_ELEMENT_SIZE)
+#define igtl_fulltdata_get_data_size(n)  ((n) * IGTL_FULLTDATA_ELEMENT_SIZE)
 
 /** igtl_tdata_get_data_n(size) calculates the number of tdatas in the body, based on
  * the body size. This function may be used when a client program parses a TDATA message. */
-#define igtl_tdata_get_data_n(size)  ((size) / IGTL_TDATA_ELEMENT_SIZE)
+#define igtl_fulltdata_get_data_n(size)  ((size) / IGTL_FULLTDATA_ELEMENT_SIZE)
 
 /** Converts endianness of each element in an array of
  *  igtl_tdata_element from host byte order to network byte order,
  *  or vice versa.*/
-void igtl_export igtl_tdata_convert_byte_order(igtl_tdata_element* tdatalist, int nelem);
-void igtl_export igtl_stt_tdata_convert_byte_order(igtl_stt_tdata* stt_tdata);
-void igtl_export igtl_rts_tdata_convert_byte_order(igtl_rts_tdata* rts_tdata);
+void igtl_export igtl_fulltdata_convert_byte_order(igtl_fulltdata_element* tdatalist, int nelem);
 
 /** Calculates CRC of TDATA, STT_TDATA and RTS_TDATA messages.*/
-igtl_uint64 igtl_export igtl_tdata_get_crc(igtl_tdata_element* tdatalist, int nelem);
-igtl_uint64 igtl_export igtl_stt_tdata_get_crc(igtl_stt_tdata* stt_tdata);
-igtl_uint64 igtl_export igtl_rts_tdata_get_crc(igtl_rts_tdata* rts_tdata);
+igtl_uint64 igtl_export igtl_fulltdata_get_crc(igtl_fulltdata_element* tdatalist, int nelem);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* __IGTL_TDATA_H */
+#endif /* __IGTL_FULLTDATA_H */
 
 
