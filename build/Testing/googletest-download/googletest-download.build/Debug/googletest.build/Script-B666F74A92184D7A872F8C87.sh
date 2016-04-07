fc328bc8a9beede2500091ed44263f755d701716
leochan2009 <leochan2009@hotmail.com>
2016-04-07 19:20:25 -0400

    files wrongly commited

--- build/Testing/googletest-download/googletest-download.build/Debug/googletest.build/Script-B666F74A92184D7A872F8C87.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/longquanchen/Desktop/googleTest/OpenIGTLink/build/Testing/googletest-download -f /Users/longquanchen/Desktop/googleTest/OpenIGTLink/build/Testing/googletest-download/CMakeScripts/googletest_cmakeRulesBuildPhase.make$CONFIGURATION all
