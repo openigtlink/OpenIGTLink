b2abdcc893a7ac1c18908fd7d18c782eec4e06c6
leochan2009 <leochan2009@hotmail.com>
2016-04-12 23:05:01 -0400

    import filename is corrected now

--- Testing/igtlSensorMessageTest.cxx
+++ Testing/igtlSensorMessageTest.cxx
@@ -16,7 +16,7 @@
 #include "igtl_header.h"
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
-#include "igtl_Sensor.h"
+#include "igtl_sensor.h"
 #include "igtlOSUtil.h"
 #include "../Testing/igtlutil/igtl_test_data_sensor.h"
 
