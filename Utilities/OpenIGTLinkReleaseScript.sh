04c470deb03d384cc0886f37549176c0c4749fa9
Junichi Tokuda <tokuda@bwh.harvard.edu>
2017-01-20 19:20:28 -0500

    script to update GitHub Pages.

--- Utilities/OpenIGTLinkReleaseScript.sh
+++ Utilities/OpenIGTLinkReleaseScript.sh
@@ -3,9 +3,16 @@
 # Bash script to release a new revision of OpenIGTLink library.
 # Run the command without arugment for the usage.
 
-GIT_URL='https://github.com/tokjun/OpenIGTLink.git'
+GITHUB_ORG="tokjun"
+GITHUB_REPO="OpenIGTLink.git"
+GITHUB_PAGES_REPO="openigtlink.github.com"
 
+GIT_URL="https://github.com/$GITHUB_ORG/$GITHUB_REPO"
+GIT_WEB_URL="https://github.com/$GITHUB_ORG/$GITHUB_PAGES_REPO"
+
+#
 # Major, minor, and patch version numbers
+#
 MAJ=0
 MIN=0
 PAT=0
@@ -20,7 +27,9 @@ printUsage() {
     echo '      patch : Patch release'
 }
 
+#
 # Extract OpenIGTLink protocol version numbers from the specified CMake file
+#
 getVersionNumbers() {
 
     local FILE=$1
@@ -34,7 +43,9 @@ getVersionNumbers() {
 
 }
 
+#
 # Update OpenIGTLink protocol version numbers in the specified CMake file
+#
 updateVersionNumbers() {
     
     local FILE=$1
@@ -49,6 +60,24 @@ updateVersionNumbers() {
 
 }
 
+
+replacePageLine() {
+    local FILE=$1
+    local STRING1=$2
+    local STRING2=$3
+    cat $FILE \
+	| sed -r 's/'"$STRING1"'.*)/'"$STRING2"'/g' > $FILE.back
+    mv $FILE.back $FILE
+}
+
+updatePage() {
+    local FILE=$1
+    local VERSION=$2
+    local STRING=$3
+    cat $FILE \
+	| sed -r 's/(\[AUTO_VER'"$VERSION"'\]\:\s+\<\>\s+\(.*\).*)/'"$STRING"'/g' > $FILE.back
+    mv $FILE.back $FILE
+}
 
 # Validate the number of arguments
 if [ "$#" -ne 1 ]; then
@@ -76,10 +105,9 @@ fi
 
 BRANCH_NAME="release-$MAJ.$MIN"
 TAG_NAME="v$MAJ.$MIN.$PAT"
-# Following line require git 1.6.3 or later
 CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 
-## Major release must be performed under the master branch
+# Major release must be performed under the master branch
 if [ $RELEASE_TYPE == "major" ]]; then
     if [ $CURRENT_BRANCH != "master" ]]; then
 	echo "ERROR: Cannot perform major release from non-master branch."
@@ -104,9 +132,45 @@ git checkout -B $BRANCH_NAME
 
 # Add release tag
 git tag -a $TAG_NAME -m "Relase $MAJ.$MIN.$PAT"    
-git push $GIT_URL $CURRENT_BRANCH
+git push --tags $GIT_URL $CURRENT_BRANCH
 
-	
+# Generate Doxygen in a temporary directory
+TEMP_DIR=`mktemp -d`
+SOURCE_DIR=`pwd`
+
+cd $TEMP_DIR
+mkdir OpenIGTLink-build
+cd OpenIGTLink-build
+cmake -DBUILD_DOCUMENTATION:BOOL=ON $SOURCE_DIR
+make Documentation
+
+# Download the repository of OpenIGTLink website
+cd $TEMP_DIR
+git clone $GIT_WEB_URL
+cd $GITHUB_PAGES_REPO
+
+# Copy Doxygen-generated API document
+mkdir api/v$MAJ.$MIN
+cp -rp $TEMP_DIR/OpenIGTLink-build/Documents/Doxygen/html/* api/v$MAJ.$MIN
+git add api/v$MAJ.$MIN/*
+git commit -a -m "Add API document for release $MAJ.$MIN.$PAT."
+
+DATE=`date +"%b %d, %G"`
+updatePage releaselog.md 0 " - $DATE : Version $MAJ.$MIN.$PAT is released."
+
+if [ $RELEASE_TYPE == "patch" ]]; then
+    replacePageLine spec.md "- [Relase $MAJ.$MIN]" "- [Release $MAJ.$MIN]](https://github.com/openigtlink/OpenIGTLink/blob/release-v$MAJ.$MIN.$PAT/Documents/Protocol/index.md)"
+    replacePageLine api/index.md "- [Relase $MAJ.$MIN]" "- [Release $MAJ.$MIN]](https://github.com/openigtlink/OpenIGTLink/blob/release-v$MAJ.$MIN.$PAT/Documents/Protocol/index.md)"
+fi
+
+if [ $RELEASE_TYPE == "minor" ]]; then
+    updatePage spec.md $MAJ " - [Release $MAJ.$MIN](https://github.com/openigtlink/OpenIGTLink/blob/release-v$MAJ.$MIN.$PAT/Documents/Protocol/index.md)"
+fi
+
+
+rm -rf $TEMP_DIR
+
+
 
 
 
