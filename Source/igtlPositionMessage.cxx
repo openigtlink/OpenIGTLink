9a69c44c1c5301c0079bc31fad4a4be60f9d70e1
Lennart Karstensen <lennart.karstensen@ipa.fraunhofer.de>
2020-04-14 17:38:45 +0200

    #222 Bugfix in PackContent, byte conversion is dependent on PackType

--- Source/igtlPositionMessage.cxx
+++ Source/igtlPositionMessage.cxx
@@ -194,8 +194,18 @@ int PositionMessage::PackContent()
     p->quaternion[3] = this->m_Quaternion[3];
     }
 
-    
-  igtl_position_convert_byte_order(p);
+  switch (this->m_PackType)
+    {
+    case POSITION_ONLY:
+      igtl_position_convert_byte_order_position_only(p);
+      break;
+    case WITH_QUATERNION3:
+      igtl_position_convert_byte_order_quaternion3(p);
+      break;
+    default: //IGTL_POSITION_MESSAGE_DEFAULT_SIZE
+      igtl_position_convert_byte_order(p);
+      break;
+    }
 
   return 1;
 }
